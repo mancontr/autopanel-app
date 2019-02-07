a079aa03bb61f20493ff1f75be7a488e32473dbe
Anxo Soto <anxo@glue.gl>
2019-02-07 17:09:00 +0100

    Updated to latest autopanel: 0.4.0

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,83 +1,10 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import { Helmet } from 'react-helmet'
-import { components } from 'autopanel'
-const {
-  AutoPanel,
-  MainLayout,
-  ProjectLayout,
-  ProjectPicker,
-  ProviderCallback,
-  Dashboard,
-  EntityTypes,
-  EntityList,
-  EntityEdit,
-  Settings
-} = components
-
-const settings = {}
-
-const AutoPanelAdapter = ({ match, history }) => {
-  const currSettings = {
-    ...settings,
-    navigate: history.push,
-    prefix: match.path
-  }
-  return (
-    <AutoPanel settings={currSettings}>
-      <Helmet>
-        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.4.0/dist/leaflet.css" />
-      </Helmet>
-      <Route path={match.path + '/providers/:provider/*'}
-        component={ProviderCallbackAdapter} />
-      <Route path={match.path} component={MainLayoutAdapter} />
-    </AutoPanel>
-  )
-}
-
-const ProviderCallbackAdapter = ({ match }) =>
-  <ProviderCallback provider={match.params.provider} />
-
-const MainLayoutAdapter = ({ match }) =>
-  <MainLayout>
-    <Route exact path={match.path} component={ProjectPicker} />
-    <Route path={match.path + '/project/:projectId'}
-      component={ProjectLayoutAdapter} />
-  </MainLayout>
-
-const ProjectLayoutAdapter = ({ match }) =>
-  <ProjectLayout projectId={match.params.projectId}>
-    <Route exact path={match.path} component={Dashboard} />
-    <Route path={match.path + '/entities'} component={EntitiesAdapter} />
-    <Route path={match.path + '/settings'} component={Settings} />
-  </ProjectLayout>
-
-const EntitiesAdapter = ({ match }) =>
-  <React.Fragment>
-    <Route exact path={match.path} component={EntityTypes} />
-    <Route path={match.path + '/:entityType'} component={EntityTypeAdapter} />
-  </React.Fragment>
-
-const EntityTypeAdapter = ({ match }) =>
-  <React.Fragment>
-    <Route exact path={match.path} component={EntityListAdapter} />
-    <Switch>
-      <Route path={match.path + '/new'} component={EntityEditAdapter} />
-      <Route path={match.path + '/:entityId'} component={EntityEditAdapter} />
-    </Switch>
-  </React.Fragment>
-
-const EntityListAdapter = ({ match }) =>
-  <EntityList entityType={match.params.entityType} />
-
-const EntityEditAdapter = ({ match }) =>
-  <EntityEdit
-    entityType={match.params.entityType}
-    entityId={match.params.entityId}
-  />
+import AutoPanelRoutes from 'autopanel/dist/routes/v4'
 
 const App = () => __CLIENT__ && (
-  <Route path="" component={AutoPanelAdapter} />
+  <Route path="" component={AutoPanelRoutes()} />
 )
 
 export default App
